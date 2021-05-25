fb85312eaec609bc4f81280a96ed87cfcd8dbbe0
SebPasieka <sebpasieka@gmail.com>
2021-05-25 14:15:16 +0200

    #15 API call to authenticate and save credentials in localStorage

--- src/SandboxApi.js
+++ src/SandboxApi.js
@@ -1,11 +1,12 @@
 import 'whatwg-fetch';
+import authService from "./Services/authService"
 
 class SandboxApi {
     fetchArticles(keyword = "") {
         const requestOptions = {
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': 'Basic '+btoa('sebastian.pasieka:9WGDbRz2Hf'),
+                'Authorization': 'Basic '+authService.getCredentials(),
             }
         };
         const url = 'https://sandbox-api.ipool.asideas.de/sandbox/api/search?types=article&publisher=welt&q='+keyword
--- src/Services/authService.js
+++ src/Services/authService.js
@@ -1,7 +1,33 @@
+import SandboxApi from '../SandboxApi'
+
+const localStorageKey = "credentials"
+
 class AuthService {
     verify(username, password) {
+        return new Promise((resolve, reject) => {
+            const requestOptions = {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Basic '+btoa(username+':'+password),
+                }
+            };
+            const url = 'https://sandbox-api.ipool.asideas.de/sandbox/api/search'
+            return fetch(url, requestOptions)
+                .then((response) => {
+                    if (response.ok) {
+                        resolve("verified")
+                        const credentials = btoa(username+':'+password)
+                        localStorage.setItem(localStorageKey, credentials)
+                    } else if (!response.ok) {
+                        reject("not verified")
+                        localStorage.removeItem(localStorageKey)
+                    }
+                })
+        })
 
-
+    }
+    getCredentials() {
+        return localStorage.getItem(localStorageKey)
     }
 
 }
