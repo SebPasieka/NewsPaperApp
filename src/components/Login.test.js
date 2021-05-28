7cd6a01d1f18af1bbb73d3e0f3bab73ce7017992
SebPasieka <sebpasieka@gmail.com>
2021-05-28 11:29:54 +0200

    update login test

--- src/components/Login.test.js
+++ src/components/Login.test.js
@@ -1,28 +1,23 @@
 import React from 'react'
-import { render, screen, act, waitFor } from "@testing-library/react"
+import { render, screen, act } from "@testing-library/react"
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom/extend-expect';
 import Login from '../components/Login'
-import { Router , Route, StaticRouter } from "react-router-dom"
-import MainContent from "../components/MainContent"
+import { Router , Route } from "react-router-dom"
 import AuthService from "../Services/authService"
 
-describe('testing login',() =>{
+describe('testing login',() => {
     it('should login user when correct credentials are given', async ()=>{
         jest.spyOn(AuthService, "verify").mockImplementation(() => Promise.resolve())
+
 
        render(
             <Router history={{location: {pathname: "/login"}, listen: jest.fn(), push: jest.fn(), createHref: jest.fn()}}>
                 <Login setAuth={jest.fn()}/>
-                <Route path="/">
-                    Hello World
-                </Route>
             </Router>
         )
 
         await act( async () => {
-            userEvent.type(screen.getByTestId('username'), 'username');
-            userEvent.type(screen.getByTestId('password'), 'password');
             userEvent.click(screen.getByTestId('submit'));
         })
 
@@ -33,8 +28,6 @@ describe('testing login',() =>{
         render(<Login setAuth={jest.fn()}/>);
 
         await act( async () => {
-            userEvent.type(screen.getByTestId('username'), 'WrongUsername');
-            userEvent.type(screen.getByTestId('password'), 'wrongPassword');
             userEvent.click(screen.getByTestId('submit'));
         })
 
