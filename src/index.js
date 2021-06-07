390a3b69dc7f758ee03707a6f5a861a4eddd9cc3
SebPasieka <sebpasieka@gmail.com>
2021-06-07 14:22:52 +0200

    basename env

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App"
+import { createBrowserHistory } from 'history'
 
+export const history = createBrowserHistory({
+    basename: process.env.PUBLIC_URL
+});
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
