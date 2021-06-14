4ae9aa5ef3d0be102a570b1852db968119a15157
SebPasieka <sebpasieka@gmail.com>
2021-06-14 15:10:18 +0200

    add testMatch

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   moduleNameMapper: {
     '\\.(css|less)$': '<rootDir>/styleJest.js',
-  }
+  },
+  "testMatch": [
+    "<rootDir>/src/components/*.test.js",
+    "<rootDir>/src/services/*.test.js",
+
+  ]
 };
