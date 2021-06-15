0291161cb760cba048a51a1566a5f110830457d3
SebPasieka <sebpasieka@gmail.com>
2021-06-15 15:28:45 +0200

    #17 implement mainContent tests

--- cypress/integration/mainContent.test.js
+++ cypress/integration/mainContent.test.js
@@ -7,4 +7,31 @@ describe('MainContent', ()=> {
 
         cy.get('[data-testid="article-entry"]', {timeout: 1000}).contains('welt')
     })
+
+    it('should display a "nothing found" message after searching with an abstract string', () => {
+        cy.visit('/')
+        cy.successfulLogin()
+
+        cy.searchArticle("UnterDieserSuchbegriffGibtEsMitSicherheitKeinArtikel")
+
+        cy.get('[data-testid="nothing-found-message"]', {timeout: 1000}).should('be.visible')
+    })
+
+    /*it('should redirect to the actual article after click on a title of an article', () => {
+        cy.visit('/')
+        cy.successfulLogin()
+
+        cy.get('[data-testid="article-link"]').first().click()
+
+        cy.url().should('contain', 'https://www.welt.de/')
+    })*/
+
+    it('should redirect to login after click on the logout button', () => {
+        cy.visit('/')
+        cy.successfulLogin()
+
+        cy.get('[data-testid="logoutButton"]').click()
+
+        cy.url().should('include', '/login')
+    })
 })
